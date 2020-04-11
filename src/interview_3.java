547c4a4f063252c1ceb2990b1619dbe0a451e7c7
miki-liao <miki.liao@linecorp.com>
2020-04-11 15:17:40 +0800

    Add interview_3

--- src/interview_3.java
+++ src/interview_3.java
@@ -1,17 +1,18 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
-public class Main {
+public class interview_3 {
 
     public static void main(String[] args) {
         int[] intArray = {2, 3, 1, 0, 2, 5, 3};
-        System.out.println(Arrays.toString(intArray));
-        String answer = "";
+        List<Integer> answer = new ArrayList<Integer>();
         for(int i = 0; i < intArray.length; i++) {
             int m = intArray[i];
             while (m != i) {
                 int tmp = intArray[m];
                 if (tmp == m){
-                    answer += tmp + ",";
+                    answer.add(tmp);
                     break;
                 } else {
                     // swap
